35403f6a9d0442570769599d7c5f92b1eeab6756
Brazesh Guragain <b.guragain1023@gmail.com>
2026-09-18 11:56:55 +1000

    completed all models for leave request

--- src/models/user/leaveRequestModel.ts
+++ src/models/user/leaveRequestModel.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import leaveRequestSchema, {
+  ILeaveRequest,
   LeaveStatus,
   LeaveType,
 } from "./leaveRequestSchema";
@@ -16,6 +17,26 @@ export interface CreateRequestInput {
   reviewComment?: string;
 }
 
-export const addLeaveRequest = (addObj: CreateRequestInput) => {
+export const addLeaveRequest = (
+  addObj: CreateRequestInput,
+): Promise<ILeaveRequest> => {
   return new leaveRequestSchema(addObj).save();
+};
+
+export const getAllLeaveRequest = (): Promise<ILeaveRequest[]> => {
+  return leaveRequestSchema.find();
+};
+
+export const getSingleLeaveRequest = (
+  requestId: string,
+): Promise<ILeaveRequest | null> => {
+  return leaveRequestSchema.findById(requestId);
+};
+
+export const updateLeaveRequest = (requestId: string, status: LeaveStatus) => {
+  return leaveRequestSchema.findByIdAndUpdate(
+    requestId,
+    { status },
+    { new: true },
+  );
 };
